7f944ee05e4eff46b50c6d838b0d056868c6fd6c
Nayar Joolfoo <nayar@joolfoo.com>
2014-08-22 21:26:06 +0400

    coming soon page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Beautiful Tomorrow</title>
+		<link rel="stylesheet" type="text/css" href="./assets/styles.css" />
+	</head>
+	<body>
+		<div class="coming-soon">
+			<span>Coming Soon...</span>
+		</div>
+	</body>
+</html>
